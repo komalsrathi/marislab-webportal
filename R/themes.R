f88b9e777296c719870843bd9c01c721c8a83c52
rathik <rathik@email.chop.edu>
2016-05-06 11:43:37 -0400

    adds function for various ggplot2 themes

---
+++ R/themes.R
@@ -0,0 +1,15 @@
+####################################
+# plot bar plot for a gene
+# Authors: Pichai Raman, Komal Rathi
+# Organization: DBHi, CHOP
+####################################
+
+themebw <- function(){
+
+	tt <- theme_bw() + theme(axis.title.x = element_text(face = "bold", size = 16),
+                           axis.text.x  = element_text(size = 14),
+                           axis.title.y = element_text(face = "bold", size = 16),
+                           axis.text.y  = element_text(size = 14),
+                           title = element_text(face = 'bold',size = 16))
+	return(tt)
+}
