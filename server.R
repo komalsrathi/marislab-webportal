572834795cc81f6b3312ccc85cdeb1ac9def0567
rathik <rathik@email.chop.edu>
2016-05-09 17:56:58 -0400

    updates server.R

--- server.R
+++ server.R
@@ -21,13 +21,36 @@ shinyServer(function(input, output, session){
   
   # load data  
   datasetInput <-  reactive({
-    file = paste('data/','2016-04-27-CellLineSTARcounts_2pass_matrix.txt',sep = '')
-    dat = read.delim(file = file)
+    file <- paste('data/','2016-04-27-CellLineSTARcounts_2pass_matrix.txt', sep = '')
+    dat <- read.delim(file = file)
   })
   
-  # output original data in tab1
+  # output original data in nbrcltable1
   output$nbrcltable1 <- DT::renderDataTable({
-    viewDataTable(datasetInput())
+    DT::datatable(datasetInput(),
+                  extensions = c('TableTools', 'ColVis','Scroller'), 
+                  options = list(dom = 'RMDCT<"clear">lfrtip', 
+                                 searchHighlight = TRUE,
+                                 initComplete = JS("function(settings, json) {",
+                                                   "$(this.api().table().header()).css({'background-color': '#005ab3', 'color': '#fff'});",
+                                                   "}"),
+                                 tableTools = list(sSwfPath = '//cdnjs.cloudflare.com/ajax/libs/datatables-tabletools/2.1.5/swf/copy_csv_xls_pdf.swf'),
+                                 pageLength = 5,
+                                 lengthMenu = list(c(5, 10, 15, 20, 25, -1), c('5', '10', '15', '20', '25', 'All')),
+                                 scrollX = TRUE),
+                  selection = 'single')
+  })
+  
+  # update all select inputs with available gene names
+  observe({
+    dat <- datasetInput()
+    n <- rownames(dat)
+    updateSelectInput(session = session, inputId = "clggcselectInput1", choices = n)
+    updateSelectInput(session = session, inputId = "clggcselectInput2", choices = n)
+    updateSelectInput(session = session, inputId = "clgeselectInput1", choices = n)
+    updateSelectInput(session = session, inputId = "clctselectInput1", choices = n)
+    updateSelectInput(session = session, inputId = "clctselectInput2", choices = n)
+    updateSelectInput(session = session, inputId = "pgehselectInput4", choices = n)
   })
   
   # output correlation plot in plot1
@@ -35,9 +58,9 @@ shinyServer(function(input, output, session){
     if(input$clggcsubmit1){
       isolate({
         dat <- datasetInput()
-        gene1 <- as.character(input$clggctextinput1)
-        gene2 <- as.character(input$clggctextinput2)
-        plotGeneScatter(dat = dat, gene1 = gene1, gene2 = gene2, customtheme = tbw)
+        gene1 <- as.character(input$clggcselectInput1)
+        gene2 <- as.character(input$clggcselectInput2)
+        plotGeneScatter(dat = dat, gene1 = `gene1`, gene2 = `gene2`, customtheme = tbw)
       })
     }
   })
@@ -46,12 +69,30 @@ shinyServer(function(input, output, session){
     if(input$clgesubmit1){
       isolate({
         dat <- datasetInput()
-        gene1 <- as.character(input$clgetextinput1)
+        gene1 <- as.character(input$clgeselectInput1)
         plotGeneBar(dat = dat, gene1 = gene1, customtheme = tbw)
       })
     }
   })
-
+  
+  # heatmap
+  # read the genes in for creating heatmaps
+  fileInput <- reactive({
+    infile <- input$clhfileInput
+    if(is.null(infile))
+      return(NULL)
+    read.table(file = infile$datapath, check.names = F, header = FALSE, stringsAsFactors = F)
+  })
+  
+  # heatmap
+  # update select input with the list of genes uploaded
+  observe({
+    dat <- fileInput()
+    n <- nrow(dat)
+    updateSelectInput(session = session, inputId = "clhselectInput1", choices = dat[,1])
+    updateSelectInput(session = session, inputId = "clhselectInput2", choices = n, selected = n)
+  })
+  
 }) # shinyServer ends
   
  
