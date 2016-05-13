4a57aea39d9cba3a28b3bf1ef3b666f021df8b38
rathik <rathik@email.chop.edu>
2016-05-13 17:01:52 -0400

    adjusts axis size

--- R/themes.R
+++ R/themes.R
@@ -6,10 +6,10 @@
 
 themebw <- function(){
 
-	tt <- theme_bw() + theme(axis.title.x = element_text(face = "bold", size = 16),
-                           axis.text.x  = element_text(size = 14),
-                           axis.title.y = element_text(face = "bold", size = 16),
-                           axis.text.y  = element_text(size = 14),
-                           title = element_text(face = 'bold',size = 16))
+	tt <- theme_bw() + theme(axis.title.x = element_text(face = "bold", size = 14),
+                           axis.text.x  = element_text(size = 12),
+                           axis.title.y = element_text(face = "bold", size = 14),
+                           axis.text.y  = element_text(size = 12),
+                           title = element_text(face = 'bold',size = 14))
 	return(tt)
 }
