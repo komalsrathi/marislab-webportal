4a6120c69b0c483be4adb231a51b1f0d8a2c5114
rathik <rathik@email.chop.edu>
2016-06-08 15:30:59 -0400

    removes .txt reference

--- R/cellMutationTable.R
+++ R/cellMutationTable.R
@@ -6,16 +6,17 @@
 
 library(reshape2)
 
-cellMutationTable <- function(gene)
+cellMutationTable <- function(gene, dataset)
 {
-	data <- read.delim("data/ExomeCalls85K8_10_13.txt", stringsAsFactors = FALSE)
-    tmpData <- data[data[,"Gene"]==gene,]
-    colstoKeep <- colnames(tmpData)[c(1:12, 42:79)]
-    tmpData2 <- melt(tmpData, id=colstoKeep)
-
-    # remove nulls
-    remove <- as.numeric(lapply(tmpData2[,"value"], FUN=nchar))!=1
-    tmpData2 <- tmpData2[remove,]
-    colnames(tmpData2)[51:52] <- c("Cell Line", "Genotype")
-    return(tmpData2)
+  data <- paste0('data/',dataset)
+  data <- read.delim(data, stringsAsFactors = FALSE)
+  tmpData <- data[data[,"Gene"]==gene,]
+  colstoKeep <- colnames(tmpData)[c(1:12, 42:79)]
+  tmpData2 <- melt(tmpData, id=colstoKeep)
+  
+  # remove nulls
+  remove <- as.numeric(lapply(tmpData2[,"value"], FUN=nchar))!=1
+  tmpData2 <- tmpData2[remove,]
+  colnames(tmpData2)[51:52] <- c("Cell Line", "Genotype")
+  return(tmpData2)
 }
