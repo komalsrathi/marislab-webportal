dbb1e07902086611e8e76d66978fde117b89f1a8
rathik <rathik@email.chop.edu>
2016-06-14 11:30:40 -0400

    add log function

--- R/plotGeneBarCNAPatientAnalysis.R
+++ R/plotGeneBarCNAPatientAnalysis.R
@@ -8,7 +8,7 @@
 load('data/allDataPatient.RData')
 
 # plot Copy Number Patient Data
-plotGeneBarCNAPatientAnalysis <- function(gene1, dataset, logby, sortby)
+plotGeneBarCNAPatientAnalysis <- function(gene1, dataset, logby, sortby, customtheme)
 {
 
   # get selected dataset
@@ -28,13 +28,18 @@ plotGeneBarCNAPatientAnalysis <- function(gene1, dataset, logby, sortby)
   if(logby == TRUE)
   {
     myData.tmp <- myData.c[,-1]
-    myData.tmp <- as.data.frame(log2(myData.tmp)-1)
+    myData.tmp <- as.data.frame(log2(myData.tmp+1))
     myData.tmp <- cbind(variable=myData.c$variable, myData.tmp)
     myData.c <- myData.tmp
   }
   colnames(myData.c)[1] <- 'Sample'
   
-  p <- ggplot(myData.c, aes_string(x='Sample', y=gene1)) + geom_bar(stat="identity") + theme(axis.text.x  = element_blank()) + ggtitle(gene1)
+  # modify gene name, dashes present
+  gene1.mut <- paste('`',gene1,'`',sep='')
+  
+  p <- ggplot(myData.c, aes_string(x='Sample', y=gene1.mut)) + 
+    customtheme + geom_bar(stat="identity") + 
+    theme(axis.text.x  = element_blank()) + ggtitle(gene1) + ylab("Copy Number\n")
   
   return(p)
 }
