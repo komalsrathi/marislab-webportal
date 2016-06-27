4236acb792b1e82a07bc169344f5e6c17685058f
rathik <rathik@email.chop.edu>
2016-06-27 16:06:33 -0400

    adds correlation dropdown

--- R/plotGeneCNAvsRNAPatientAnalysis.R
+++ R/plotGeneCNAvsRNAPatientAnalysis.R
@@ -5,7 +5,7 @@
 #############################################
 
 # plot scatter plot of 2 genes
-plotGeneCNAvsRNAPatientAnalysis <- function(gene1, myData, mycData, customtheme)
+plotGeneCNAvsRNAPatientAnalysis <- function(gene1, myData, mycData, customtheme, correlation)
 {
   
   #Let's do other stuff
@@ -14,10 +14,23 @@ plotGeneCNAvsRNAPatientAnalysis <- function(gene1, myData, mycData, customtheme)
   colnames(tmpDataGC) <- c("mRNA", "CNA")
   
   #For title correlation and p-value
-  tmpcor <- cor.test(tmpDataGC[,"mRNA"], tmpDataGC[,"CNA"])
-  tmpcorp <- round(tmpcor$p.val,2)
-  tmpcor <- round(tmpcor$estimate,2)
-  myText <- paste("Cor=", tmpcor, " | P-Val=", tmpcorp, sep="")
+  tmpcor <- cor.test(tmpDataGC[,"mRNA"], tmpDataGC[,"CNA"], method = correlation)
+  if(tmpcor$p.value==0){
+    tmpcorp <- '< 2.2e-16'
+  }
+  if(tmpcor$p.value>0){
+    tmpcorp <- format(tmpcor$p.value, scientific = T)
+  }
+  if(tmpcor$estimate==1){
+    tmpcore <- 1
+  }
+  if(tmpcor$estimate!=1){
+    tmpcore <- format(tmpcor$estimate, scientific = T)
+  }
+  myText <- paste("Cor=", tmpcore, " | P-Val=", tmpcorp, sep="")
+  #tmpcorp <- round(tmpcor$p.val,2)
+  #tmpcor <- round(tmpcor$estimate,2)
+  #myText <- paste("Cor=", tmpcor, " | P-Val=", tmpcorp, sep="")
 
   tmpDataGC[,"CL_NAME"] <- rownames(tmpDataGC)
   
@@ -27,6 +40,8 @@ plotGeneCNAvsRNAPatientAnalysis <- function(gene1, myData, mycData, customtheme)
   p <- plotly_build(p)
   p$layout$xaxis$title <- paste0("mRNA"," (RMA)")
   p$layout$yaxis$title <- paste0("CNA"," (Copy Number)")
+  p$data[[1]]$marker$line$color <- "rgb(220,20,60)"
+  p$data[[1]]$marker$color <- "rgb(220,20,60)"
   
   return(p)
 }
