e140946c854a29a3f629c24b9be498391e7ead76
rathik <rathik@email.chop.edu>
2016-07-08 13:30:55 -0400

    adds GPL Patient dataset

--- R/kapmChoose.R
+++ R/kapmChoose.R
@@ -6,24 +6,33 @@
 
 source('R/kapmPlot.R')
 
-kapmChoose <- function(datatype, dataset, genes, endpoint="os")
+kapmChoose <- function(datatype, dataset, genes, endpoint)
 {
   if(datatype=="NB88" & endpoint=="os")
   {
     out <- kapmPlot(genes, dataset, createPlot=T, tVar="nti_surv_overall", eVar="nti_event_overall_num")
   }
-  if(datatype=="IH250" & endpoint=="os")
-  {
-    out <- kapmPlot(genes, dataset, createPlot=T, tVar="stime", eVar="scens")
-  }
   if(datatype=="NB88" & endpoint=="efs")
   {
     out <- kapmPlot(genes, dataset, createPlot=T, tVar="nti_surv_progrfree", eVar="nti_event_progrfree_num")
+  }
+  if(datatype=="IH250" & endpoint=="os")
+  {
+    out <- kapmPlot(genes, dataset, createPlot=T, tVar="stime", eVar="scens")
   }
   if(datatype=="IH250" & endpoint=="efs")
   {
     out <- kapmPlot(genes, dataset, createPlot=T, tVar="efstime", eVar="efscens")
   }
+  if(datatype=="GPL" & endpoint=="os")
+  {
+    out <- kapmPlot(genes, dataset, createPlot=T, tVar="stime", eVar="scens")
+  }
+  if(datatype=="GPL" & endpoint=="efs")
+  {
+    out <- kapmPlot(genes, dataset, createPlot=T, tVar="efstime", eVar="efscens")
+  }
+  
   
   out <- ggplotly(out)
   return(out)
