bace7ce42f97ec4da276d6e65ededbf9afbed367
rathik <rathik@email.chop.edu>
2016-07-13 11:29:03 -0400

    adds more info to dataset summary

--- R/dbplot.R
+++ R/dbplot.R
@@ -1,8 +1,20 @@
 dbplot <- function(data_summary){
-  data_summary <- data_summary[order(data_summary$Type, data_summary$Source, data_summary$Genes),]
-  data_summary$Source <- factor(data_summary$Source, levels = as.character(data_summary$Source))
-  p <- plotly_build(ggplot(data = data_summary, aes(x = Source, y = Genes)) +
-                      geom_bar(stat="identity",aes(color = Type)) +
+  # data_summary <- data_summary[order(data_summary$Type, data_summary$Source, data_summary$Genes),]
+  # data_summary$Source <- factor(data_summary$Source, levels = as.character(data_summary$Source))
+  # p <- plotly_build(ggplot(data = data_summary, aes(x = Source, y = Genes)) +
+  #                     geom_bar(stat="identity",aes(color = Type)) +
+  #                     theme_bw() + geom_text(aes(label = Samples, color = Type), size=5) +
+  #                     theme_hc(bgcolor = "darkunica") +
+  #                     scale_colour_hc("darkunica") +
+  #                     theme(axis.title = element_blank(),
+  #                           axis.ticks = element_blank(),
+  #                           axis.text.x = element_text(angle = -45, hjust = 0),
+  #                           panel.grid = element_blank(),
+  #                           plot.margin = unit(c(0.5, 0.5, 2, 0.5), "cm")))
+  data_summary <- data_summary[order(data_summary$Type, data_summary$Dataset, data_summary$Genes),]
+  data_summary$Dataset <- factor(data_summary$Dataset, levels = as.character(data_summary$Dataset))
+  p <- plotly_build(ggplot(data = data_summary, aes(x = Dataset, y = Genes)) +
+                      geom_bar(stat="identity",aes(color = Type, line = Source)) +
                       theme_bw() + geom_text(aes(label = Samples, color = Type), size=5) +
                       theme_hc(bgcolor = "darkunica") +
                       scale_colour_hc("darkunica") +
@@ -10,7 +22,7 @@ dbplot <- function(data_summary){
                             axis.ticks = element_blank(),
                             axis.text.x = element_text(angle = -45, hjust = 0),
                             panel.grid = element_blank(),
-                            plot.margin = unit(c(0.5, 0.5, 2, 0.5), "cm")))
+                            plot.margin = unit(c(0.5, 0.5, 2.5, 0.5), "cm")))
   p$layout$plot_bgcolor <- p$layout$paper_bgcolor
   p$layout$xaxis$tickfont$size <- 16
   p$layout$yaxis$tickfont$size <- 16
