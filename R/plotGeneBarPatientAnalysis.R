186476c910ae5e95007a004769046a113cd2decf
rathik <rathik@email.chop.edu>
2016-07-26 11:06:42 -0400

    adds FPKM as a datatype

--- R/plotGeneBarPatientAnalysis.R
+++ R/plotGeneBarPatientAnalysis.R
@@ -5,7 +5,7 @@
 #############################################
 
 # function to plot bar charts
-plotGeneBarPatientAnalysis <- function(gene1, myDataExp, myDataAnn, sortby, log, density, colorby, customtheme)
+plotGeneBarPatientAnalysis <- function(datatype, gene1, myDataExp, myDataAnn, sortby, log, density, colorby, customtheme)
 {
 
   # get expression and annotation of the selected dataset
@@ -13,29 +13,47 @@ plotGeneBarPatientAnalysis <- function(gene1, myDataExp, myDataAnn, sortby, log,
   myDataExp$gene <- rownames(myDataExp)
   myDataExp.m <- melt(data = myDataExp, id.vars = 'gene')
   myDataExp.c <- dcast(data = myDataExp.m, formula = variable~gene, value.var = 'value')
-  colnames(myDataExp.c)[1] = "Sample"
+  colnames(myDataExp.c)[1] = "Patient_Sample"
   
   # sort by value
   if(sortby == TRUE){
-    myDataExp.c$Sample <- reorder(myDataExp.c$Sample,myDataExp.c[,gene1])
+    myDataExp.c$Patient_Sample <- reorder(myDataExp.c$Patient_Sample,myDataExp.c[,gene1])
   }
   
   # plot log values
-  if(log==FALSE)
+  if(length(grep('FPKM',datatype))==0)
   {
-    y.axis <- "RMA"
-    myDataExp.tmp <- myDataExp.c[,-1]
-    myDataExp.tmp <- as.data.frame(2^myDataExp.tmp)
-    myDataExp.tmp <- cbind(Sample=myDataExp.c$Sample, myDataExp.tmp)
-    myDataExp.c <- myDataExp.tmp
+    if(log==FALSE)
+    {
+      y.axis <- "RMA"
+      myDataExp.tmp <- myDataExp.c[,-1]
+      myDataExp.tmp <- as.data.frame(2^myDataExp.tmp)
+      myDataExp.tmp <- cbind(Patient_Sample=myDataExp.c$Patient_Sample, myDataExp.tmp)
+      myDataExp.c <- myDataExp.tmp
+    }
+    if(log==TRUE)
+    {
+      y.axis <- "log2(RMA)"
+    }
   }
-  if(log==TRUE)
+  if(length(grep('FPKM',datatype))==1)
   {
-    y.axis <- "log2(RMA)"
+    if(log==FALSE)
+    {
+      y.axis <- "FPKM"
+    }
+    if(log==TRUE)
+    {
+      y.axis <- "log2(FPKM)"
+      myDataExp.tmp <- myDataExp.c[,-1]
+      myDataExp.tmp <- as.data.frame(log2(myDataExp.tmp+1))
+      myDataExp.tmp <- cbind(Patient_Sample=myDataExp.c$Patient_Sample, myDataExp.tmp)
+      myDataExp.c <- myDataExp.tmp
+    }
   }
   
   # add annotation data to expression set
-  myDataExp.c <- merge(myDataExp.c, myDataAnn, by.x="Sample",by.y='row.names')
+  myDataExp.c <- merge(myDataExp.c, myDataAnn, by.x="Patient_Sample",by.y='row.names')
   
   # eliminate confusion between MYCN gene and status
   if(length(grep('MYCN',colnames(myDataExp.c)))>1)
@@ -59,7 +77,7 @@ plotGeneBarPatientAnalysis <- function(gene1, myDataExp, myDataAnn, sortby, log,
   {
     if(density == FALSE)
     {
-      p <- ggplot(myDataExp.c, aes_string(x='Sample', y=gene1.mut)) + 
+      p <- ggplot(myDataExp.c, aes_string(x='Patient_Sample', y=gene1.mut)) + 
         geom_bar(stat="identity") + customtheme + theme(axis.text.x  = element_text(angle=90)) + ggtitle(gene1)
     }
     if(density == TRUE)
@@ -71,7 +89,7 @@ plotGeneBarPatientAnalysis <- function(gene1, myDataExp, myDataAnn, sortby, log,
   {
     if(density == FALSE)
     {
-      p <- ggplot(myDataExp.c, aes_string(x='Sample', y=gene1.mut, fill=colorby)) + customtheme +
+      p <- ggplot(myDataExp.c, aes_string(x='Patient_Sample', y=gene1.mut, fill=colorby)) + customtheme +
         geom_bar(stat="identity") + theme(axis.text.x  = element_text(angle=90)) + ggtitle(gene1)
     }
     if(density == TRUE)
