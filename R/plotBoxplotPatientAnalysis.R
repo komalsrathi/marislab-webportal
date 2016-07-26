cb5ff361dfcc3eb2a2cd749ae735b1740e3ff6ef
rathik <rathik@email.chop.edu>
2016-07-26 11:06:36 -0400

    adds FPKM as a datatype

--- R/plotBoxplotPatientAnalysis.R
+++ R/plotBoxplotPatientAnalysis.R
@@ -4,31 +4,50 @@
 # Organization: DBHi, CHOP
 #############################################
 
-plotBoxplotPatientAnalysis <- function(gene1, colorby, myDataExp, myDataAnn, log, customtheme)
+plotBoxplotPatientAnalysis <- function(datatype, gene1, colorby, myDataExp, myDataAnn, log, customtheme)
 {
   # get expression and annotation of the selected dataset
   # modify dataframe
   myDataExp$gene <- rownames(myDataExp)
   myDataExp.m <- melt(data = myDataExp, id.vars = 'gene')
   myDataExp.c <- dcast(data = myDataExp.m, formula = variable~gene, value.var = 'value')
-  colnames(myDataExp.c)[1] = "Cell_Line"
+  colnames(myDataExp.c)[1] = "Patient_Sample"
   
   # plot log values
-  if(log==FALSE)
+  if(length(grep('FPKM',datatype))==0)
   {
-    y.axis <- "RMA"
-    myDataExp.tmp <- myDataExp.c[,-1]
-    myDataExp.tmp <- as.data.frame(2^myDataExp.tmp)
-    myDataExp.tmp <- cbind(Cell_Line=myDataExp.c$Cell_Line, myDataExp.tmp)
-    myDataExp.c <- myDataExp.tmp
+    if(log==FALSE)
+    {
+      y.axis <- "RMA"
+      myDataExp.tmp <- myDataExp.c[,-1]
+      myDataExp.tmp <- as.data.frame(2^myDataExp.tmp)
+      myDataExp.tmp <- cbind(Patient_Sample=myDataExp.c$Patient_Sample, myDataExp.tmp)
+      myDataExp.c <- myDataExp.tmp
+    }
+    if(log==TRUE)
+    {
+      y.axis <- "log2(RMA)"
+    }
   }
-  if(log==TRUE)
+  if(length(grep('FPKM',datatype))==1)
   {
-    y.axis <- "log2(RMA)"
+    if(log==FALSE)
+    {
+      y.axis <- "FPKM"
+    }
+    if(log==TRUE)
+    {
+      y.axis <- "log2(FPKM)"
+      myDataExp.tmp <- myDataExp.c[,-1]
+      myDataExp.tmp <- as.data.frame(log2(myDataExp.tmp+1))
+      myDataExp.tmp <- cbind(Patient_Sample=myDataExp.c$Patient_Sample, myDataExp.tmp)
+      myDataExp.c <- myDataExp.tmp
+    }
   }
   
+  
   # add annotation data to expression set
-  myDataExp.c <- merge(myDataExp.c, myDataAnn, by.x="Cell_Line",by.y='row.names')
+  myDataExp.c <- merge(myDataExp.c, myDataAnn, by.x="Patient_Sample",by.y='row.names')
   
   # eliminate confusion between MYCN gene and status
   if(length(grep('MYCN',colnames(myDataExp.c)))>1)
