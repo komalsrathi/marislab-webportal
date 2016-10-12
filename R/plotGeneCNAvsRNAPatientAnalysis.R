2d261e582ec2388e93d3ea8947db5e9c48203207
rathik <rathik@email.chop.edu>
2016-10-12 15:28:00 -0400

    updates Y axis label

--- R/plotGeneCNAvsRNAPatientAnalysis.R
+++ R/plotGeneCNAvsRNAPatientAnalysis.R
@@ -28,9 +28,6 @@ plotGeneCNAvsRNAPatientAnalysis <- function(gene1, myData, mycData, customtheme,
     tmpcore <- format(tmpcor$estimate, scientific = T)
   }
   myText <- paste("Cor=", tmpcore, " | P-Val=", tmpcorp, sep="")
-  #tmpcorp <- round(tmpcor$p.val,2)
-  #tmpcor <- round(tmpcor$estimate,2)
-  #myText <- paste("Cor=", tmpcor, " | P-Val=", tmpcorp, sep="")
 
   tmpDataGC[,"CL_NAME"] <- rownames(tmpDataGC)
   
@@ -38,10 +35,10 @@ plotGeneCNAvsRNAPatientAnalysis <- function(gene1, myData, mycData, customtheme,
     geom_point() + geom_smooth(method="lm") + ggtitle(myText)
   
   p <- plotly_build(p)
-  p$layout$xaxis$title <- paste0("mRNA"," (RMA)")
-  p$layout$yaxis$title <- paste0("CNA"," (Copy Number)")
-  p$data[[1]]$marker$line$color <- "rgb(220,20,60)"
-  p$data[[1]]$marker$color <- "rgb(220,20,60)"
+  p$x$layout$xaxis$title <- paste0("mRNA"," (RMA)")
+  p$x$layout$yaxis$title <- paste0("CNA"," (Copy Number)")
+  #p$x$data[[1]]$marker$line$color <- "rgb(220,20,60)"
+  #p$x$data[[1]]$marker$color <- "rgb(220,20,60)"
   
   return(p)
 }
