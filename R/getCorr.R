b3d58fef263168a7aaed02a147eb7a3a2afb754e
rathik <rathik@email.chop.edu>
2017-05-12 16:26:00 -0400

    add function for correlations

--- R/getCorr.R
+++ R/getCorr.R
@@ -1,10 +1,22 @@
 
 getCorr <- function(dat, gene1, gene2, correlation){
   
+  if(nrow(dat)<=2){
+    cor.title <- data.frame(Cor = NA, Pval = NA)
+    return(cor.title)
+  }
+  
   x <- dat[,gene1]
   y <- dat[,gene2]
   
   cor <- cor.test(x = x, y = y, method = correlation)
+  
+  if(is.na(cor$p.value) & is.na(cor$estimate)){
+    cor.pval <- NA
+    cor.est <- NA
+    cor.title <- data.frame(Cor = NA, Pval = NA)
+    return(cor.title)
+  }
   
   if(cor$p.value==0){
     cor.pval <- '< 2.2e-16'
