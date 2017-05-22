e994ca917227bd5d7e53a776d7e497215d9d4a56
rathik <rathik@email.chop.edu>
2017-05-22 11:30:06 -0400

    updates gene correlations patient data

--- R/plotGeneScatterPatientData.R
+++ R/plotGeneScatterPatientData.R
@@ -7,98 +7,84 @@
 # plot scatter plot of 2 genes##################
 plotGeneScatterPatientData <- function(datatype, gene1, gene2, myDataExp, myDataAnn, log, colorby, correlation, customtheme)
 {
-
+  
   # get expression and annotation of the selected dataset
   myDataExp <- myDataExp[rownames(myDataExp) %in% c(gene1, gene2),]
-	myDataExp$gene <- rownames(myDataExp)
-	myDataExp.m <- melt(data = myDataExp, id.vars = 'gene')
-	myDataExp.c <- dcast(data = myDataExp.m, formula = variable~gene, value.var = 'value')
-	colnames(myDataExp.c)[1] = "Sample"
-	
-	#For title correlation and p-value
-	cor <- cor.test(myDataExp.c[,gene1], myDataExp.c[,gene2], method = correlation)
-	if(is.na(cor$p.value)){
-	  cor.pval <- NA
-	} else if(cor$p.value==0){
-	  cor.pval <- '< 2.2e-16'
-	} else if(cor$p.value>0){
-	  cor.pval <- format(cor$p.value, scientific = T, digits = 3)
-	} 
-	
-	if(is.na(cor$estimate)){
-	  cor.est <- NA
-	} else if(cor$estimate==1){
-	  cor.est <- 1
-	} else if(cor$estimate!=1){
-	  cor.est <- format(cor$estimate, scientific = T, digits = 3)
-	}
-	cor.title <- paste("Cor = ", cor.est, " | P-Val = ", cor.pval, sep="")
-
-	# modify gene name, dashes present
-	gene1.mut <- paste('`',gene1,'`',sep = '')
-	gene2.mut <- paste('`',gene2,'`',sep = '')
-	
-	# plot log values? 
-	if(length(grep('FPKM',datatype))==0)
-	{
-	  if(log==FALSE)
-	  {
-	    y.axis <- "RMA"
-	    myDataExp.c[,gene1] <- 2^(myDataExp.c[,gene1])
-	  }
-	  if(log==TRUE)
-	  {
-	    y.axis <- "log2(RMA)"
-	  }
-	}
-	if(length(grep('FPKM',datatype))==1)
-	{
-	  if(log==FALSE)
-	  {
-	    y.axis <- "FPKM"
-	  }
-	  if(log==TRUE)
-	  {
-	    y.axis <- "log2(FPKM)"
-	    myDataExp.c <- log2(myDataExp.c[,gene1]+1)
-	  }
-	}
-	
-	# add annotation data to expression set
-	myDataExp.c <- merge(myDataExp.c, myDataAnn, by.x = "Sample", by.y = 'row.names')
-	if(colorby != "None"){
-	  correlations <- plyr::ddply(.data = myDataExp.c, .variables = colorby, .fun = function(x) getCorr(dat = x, gene1 = gene1, gene2 = gene2, correlation = correlation))
-	} else {
-	  correlations <- data.frame(Cor = cor.est, Pval = cor.pval)
-	}
-	
-	# eliminate confusion between MYCN gene and status
-	if(length(grep('MYCN',colnames(myDataExp.c)))>1)
-	{
-	  coln <- grep("MYCN.x", colnames(myDataExp.c))
-	  colnames(myDataExp.c)[coln] <- 'MYCN'
-	  coln <- grep("MYCN.y", colnames(myDataExp.c))
-	  colnames(myDataExp.c)[coln] <- 'MYCNS'
-	  if(colorby=="MYCN")
-	  {
-	    colorby = "MYCNS"
-	  }
-	}
-	
-	# plot
-	if(colorby == "None"){
-	  p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'Sample')) + 
-	    geom_point() + geom_smooth(method = lm) + customtheme + ggtitle(cor.title)
-	}
-	if(colorby != "None"){
-	  p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'Sample', color = colorby)) + 
-	    geom_point() + geom_smooth(method = lm) + customtheme + ggtitle(cor.title)
-	}
-	
-	p <- plotly_build(p)
-	p$x$layout$yaxis$title <- paste0(gene2,' (', y.axis,')')
-	p$x$layout$xaxis$title <- paste0(gene1,' (', y.axis,')')
-	
-	newList <- list(p, correlations)
-	return(newList)
+  myDataExp$gene <- rownames(myDataExp)
+  myDataExp.m <- melt(data = myDataExp, id.vars = 'gene')
+  myDataExp.c <- dcast(data = myDataExp.m, formula = variable~gene, value.var = 'value')
+  colnames(myDataExp.c)[1] = "Sample"
+  
+  #For title correlation and p-value
+  cor <- cor.test(myDataExp.c[,gene1], myDataExp.c[,gene2], method = correlation)
+  if(is.na(cor$p.value)){
+    cor.pval <- NA
+  } else if(cor$p.value==0){
+    cor.pval <- '< 2.2e-16'
+  } else if(cor$p.value>0){
+    cor.pval <- format(cor$p.value, scientific = T, digits = 3)
+  } 
+  
+  if(is.na(cor$estimate)){
+    cor.est <- NA
+  } else if(cor$estimate==1){
+    cor.est <- 1
+  } else if(cor$estimate!=1){
+    cor.est <- format(cor$estimate, scientific = T, digits = 3)
+  }
+  cor.title <- paste("Cor = ", cor.est, " | P-Val = ", cor.pval, sep="")
+  
+  # modify gene name, dashes present
+  gene1.mut <- paste('`',gene1,'`',sep = '')
+  gene2.mut <- paste('`',gene2,'`',sep = '')
+  
+  # plot log values? 
+  if(length(grep('FPKM',datatype))==0) {
+    if(log==FALSE) {
+      y.axis <- "RMA"
+      myDataExp.c[,c(gene1,gene2)] <- 2^(myDataExp.c[,c(gene1,gene2)])
+    } else {
+      y.axis <- "log2(RMA)"
+    }
+  } else {
+    if(log==FALSE)
+    {
+      y.axis <- "FPKM"
+    } else {
+      y.axis <- "log2(FPKM)"
+      myDataExp.c[,c(gene1,gene2)] <- log2(myDataExp.c[,c(gene1,gene2)]+1)
+    }
+  }
+  
+  # add annotation data to expression set
+  myDataExp.c <- merge(myDataExp.c, myDataAnn, by.x = "Sample", by.y = 'row.names')
+  
+  # eliminate confusion between MYCN gene and status
+  if(length(grep('MYCN',colnames(myDataExp.c)))>1) {
+    coln <- grep("MYCN.x", colnames(myDataExp.c))
+    colnames(myDataExp.c)[coln] <- 'MYCN'
+    coln <- grep("MYCN.y", colnames(myDataExp.c))
+    colnames(myDataExp.c)[coln] <- 'MYCNS'
+    if(colorby=="MYCN")
+    {
+      colorby = "MYCNS"
+    }
+  }
+  
+  if(colorby != "None"){
+    correlations <- plyr::ddply(.data = myDataExp.c, .variables = colorby, .fun = function(x) getCorr(dat = x, gene1 = gene1, gene2 = gene2, correlation = correlation))
+    p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'Sample', color = colorby)) + 
+      geom_point() + geom_smooth(method = lm) + customtheme + ggtitle(cor.title)
+  } else {
+    correlations <- data.frame(Cor = cor.est, Pval = cor.pval)
+    p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'Sample')) + 
+      geom_point() + geom_smooth(method = lm) + customtheme + ggtitle(cor.title)
+  }
+  
+  p <- plotly_build(p)
+  p$x$layout$yaxis$title <- paste0(gene2,' (', y.axis,')')
+  p$x$layout$xaxis$title <- paste0(gene1,' (', y.axis,')')
+  
+  newList <- list(p, correlations)
+  return(newList)
 } 
