bb2192216cb6c2d93768a431627ed5f6e937d7b9
rathik <rathik@email.chop.edu>
2019-02-21 09:57:07 -0500

    update copy number data

--- R/plotGeneBarCNAPatientAnalysis.R
+++ R/plotGeneBarCNAPatientAnalysis.R
@@ -5,23 +5,24 @@
 #############################################
 
 # plot Copy Number Patient Data
-plotGeneBarCNAPatientAnalysis <- function(gene1, myData, logby, sortby, customtheme)
+plotGeneBarCNAPatientAnalysis <- function(gene1, myData, sortby, logby, customtheme, phenotype, colorby)
 {
-
+  
   # get selected dataset
   myData <- myData[rownames(myData) %in% gene1,]
   myData$gene <- rownames(myData)
   myData.m <- melt(data = myData, id.vars = 'gene')
   myData.c <- dcast(data = myData.m, formula = variable~gene, value.var = 'value')
   
-  if(sortby == TRUE)
-  {
-    myData.c <- myData.c[order(myData.c[,gene1]),]
+  # add phenotype data - MYCN status
+  myData.c <- merge(myData.c, phenotype, by.x = 'variable', by.y = 'TARGET.USI', all.x = TRUE)
+  
+  if(sortby != "None") {
+    myData.c <- myData.c[order(myData.c[,sortby]),]
     myData.c$variable <- factor(myData.c$variable, levels=myData.c$variable)
   }
   
-  if(logby == TRUE)
-  {
+  if(logby == TRUE) {
     myData.c[,gene1] <- log2(myData.c[,gene1]-1)
   }
   colnames(myData.c)[1] <- 'Sample'
@@ -29,15 +30,28 @@ plotGeneBarCNAPatientAnalysis <- function(gene1, myData, logby, sortby, customth
   # modify gene name, dashes present
   gene1.mut <- paste('`',gene1,'`',sep='')
   
-  p <- ggplot(myData.c, aes_string(x='Sample', y=gene1.mut)) + 
-    customtheme + geom_bar(stat="identity") + 
-    theme(axis.text.x  = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.text.y = element_text(size = 16),
-          axis.title.y= element_text(size = 16),
-          axis.title.x = element_text(size = 16),
-          plot.title = element_text(size = 18)) + ggtitle(gene1) + 
-    ylab("Copy Number\n") + xlab("\nSample")
+  if(colorby == "None"){
+    p <- ggplot(myData.c, aes_string(x='Sample', y = gene1.mut)) + 
+      customtheme + geom_bar(stat="identity") + 
+      theme(axis.text.x  = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.text.y = element_text(size = 16),
+            axis.title.y= element_text(size = 16),
+            axis.title.x = element_text(size = 16),
+            plot.title = element_text(size = 18)) + ggtitle(gene1) + 
+      ylab("Copy Number\n") + xlab("\nSample")
+  } else {
+    p <- ggplot(myData.c, aes_string(x = 'Sample', y = gene1.mut, fill = colorby)) + 
+      customtheme + geom_bar(stat="identity") + 
+      theme(axis.text.x  = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.text.y = element_text(size = 16),
+            axis.title.y= element_text(size = 16),
+            axis.title.x = element_text(size = 16),
+            plot.title = element_text(size = 18)) + ggtitle(gene1) + 
+      ylab("Copy Number\n") + xlab("\nSample")
+  }
+  
   
   # p <- plotly_build(p)
   
