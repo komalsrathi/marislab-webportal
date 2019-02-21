ba9946ddc180e3145ffd6a35aec05e75c35c6522
rathik <rathik@email.chop.edu>
2019-02-21 09:56:59 -0500

    updates PPTC study

--- R/plotGeneScatterPPTC.R
+++ R/plotGeneScatterPPTC.R
@@ -11,6 +11,7 @@
 # tumor <- c('Neuroblastoma', 'Ependymoma', 'Medulloblastoma')
 # myDataAnn <- get(load('data/PPTC_FPKM_hg19_Wheeler_subtracted_mData.RData'))
 # correlation <- 'pearson'
+source('R/getCorr.R')
 
 # plot scatter plot of 2 genes ##################
 plotGeneScatterPPTC <- function(gene1, gene2, myDataExp, myDataAnn, log, tumor, correlation, customtheme, colorby)
@@ -20,17 +21,17 @@ plotGeneScatterPPTC <- function(gene1, gene2, myDataExp, myDataAnn, log, tumor, 
   # if(length(tumor) == 1 && tumor == "none"){
   #   colorby <- "None"
   # } else if(length(tumor) >= 1 && tumor != "none"){
-  #   myDataAnn <- myDataAnn[which(myDataAnn$CANCER %in% tumor),]
-  #   colorby <- 'CANCER'
+  #   myDataAnn <- myDataAnn[which(myDataAnn$CANCER_TYPE %in% tumor),]
+  #   colorby <- 'CANCER_TYPE'
   # }  
   if(length(tumor) >= 1 && tumor != "none"){
-    myDataAnn <- myDataAnn[which(myDataAnn$CANCER %in% tumor),]
+    myDataAnn <- myDataAnn[which(myDataAnn$CANCER_TYPE %in% tumor),]
   }  
   myDataExp <- myDataExp[rownames(myDataExp) %in% c(gene1, gene2),colnames(myDataExp) %in% rownames(myDataAnn)]
   myDataExp$gene <- rownames(myDataExp)
   myDataExp.m <- melt(data = myDataExp, id.vars = 'gene')
   myDataExp.c <- dcast(data = myDataExp.m, formula = variable~gene, value.var = 'value')
-  colnames(myDataExp.c)[1] = "SAMPLE_ID"
+  colnames(myDataExp.c)[1] = "MODEL"
   
   #For title correlation and p-value
   cor <- cor.test(myDataExp.c[,gene1], myDataExp.c[,gene2], method = correlation)
@@ -64,16 +65,17 @@ plotGeneScatterPPTC <- function(gene1, gene2, myDataExp, myDataAnn, log, tumor, 
   }
   
   # add annotation data to expression set
-  myDataExp.c <- merge(myDataExp.c, myDataAnn, by = "SAMPLE_ID")
+  myDataExp.c <- merge(myDataExp.c, myDataAnn, by = "MODEL")
   
   # colorby tumor
   if(colorby != "None"){
     correlations <- plyr::ddply(.data = myDataExp.c, .variables = colorby, .fun = function(x) getCorr(dat = x, gene1 = gene1, gene2 = gene2, correlation = correlation))
-    p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'SAMPLE_ID', color = colorby)) + 
+    print(correlations)
+    p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'MODEL', color = colorby)) + 
       geom_point() + geom_smooth(method = lm) + customtheme + ggtitle(cor.title)
   } else {
     correlations <- data.frame(Cor = cor.est, Pval = cor.pval)
-    p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'SAMPLE_ID')) + 
+    p <- ggplot(data = myDataExp.c, aes_string(x = gene1.mut, y = gene2.mut, label = 'MODEL')) + 
       geom_point() + geom_smooth(method = lm) + customtheme + ggtitle(cor.title)
   }
   
