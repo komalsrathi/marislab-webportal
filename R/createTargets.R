63efe2223abe68a924d0c53d49834a4c82d2f85a
rathik <rathik@email.chop.edu>
2019-05-10 14:27:32 -0400

    udpate cell line comparison tool

--- R/createTargets.R
+++ R/createTargets.R
@@ -4,9 +4,7 @@
 # Organization: DBHi, CHOP
 ##############################
 
-createTargets <- function(list1, list2)
-{
-  
+createTargets <- function(list1, list2){
   class <- c(rep("SET1", length(list1)), rep("SET2", length(list2)))
   mydf <- data.frame(c(list1, list2), class)
   rownames(mydf) <- mydf[,1]
