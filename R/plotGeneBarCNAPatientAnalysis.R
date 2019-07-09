f2c6e45bbb748bb6d585f5da88aec34264d2603d
rathik <rathik@email.chop.edu>
2019-07-09 15:01:43 -0400

    fix p-values

--- R/plotGeneBarCNAPatientAnalysis.R
+++ R/plotGeneBarCNAPatientAnalysis.R
@@ -15,7 +15,7 @@ plotGeneBarCNAPatientAnalysis <- function(gene1, myData, sortby, logby, customth
   myData.c <- dcast(data = myData.m, formula = variable~gene, value.var = 'value')
   
   # add phenotype data - MYCN status
-  myData.c <- merge(myData.c, phenotype, by.x = 'variable', by.y = 'TARGET.USI', all.x = TRUE)
+  myData.c <- merge(myData.c, phenotype, by.x = 'variable', by.y = 'row.names', all.x = TRUE)
   
   if(sortby == "None") {
     myData.c <- myData.c[order(myData.c[,gene1]),]
@@ -28,13 +28,13 @@ plotGeneBarCNAPatientAnalysis <- function(gene1, myData, sortby, logby, customth
   if(logby == TRUE) {
     myData.c[,gene1] <- log2(myData.c[,gene1]-1)
   }
-  colnames(myData.c)[1] <- 'Sample'
+  colnames(myData.c)[1] <- 'Patient_Sample'
   
   # modify gene name, dashes present
   gene1.mut <- paste('`',gene1,'`',sep='')
   
   if(colorby == "None"){
-    p <- ggplot(myData.c, aes_string(x='Sample', y = gene1.mut)) + 
+    p <- ggplot(myData.c, aes_string(x='Patient_Sample', y = gene1.mut)) + 
       customtheme + geom_bar(stat="identity") + 
       theme(axis.text.x  = element_blank(),
             axis.ticks.x = element_blank(),
@@ -44,7 +44,7 @@ plotGeneBarCNAPatientAnalysis <- function(gene1, myData, sortby, logby, customth
             plot.title = element_text(size = 18)) + ggtitle(gene1) + 
       ylab("Copy Number\n") + xlab("\nSample")
   } else {
-    p <- ggplot(myData.c, aes_string(x = 'Sample', y = gene1.mut, fill = colorby)) + 
+    p <- ggplot(myData.c, aes_string(x = 'Patient_Sample', y = gene1.mut, fill = colorby)) + 
       customtheme + geom_bar(stat="identity") + 
       theme(axis.text.x  = element_blank(),
             axis.ticks.x = element_blank(),
