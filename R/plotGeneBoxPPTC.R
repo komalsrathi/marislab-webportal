d9010f517ad8f14b7257cc755df8e9830b20bfdf
rathik <rathik@email.chop.edu>
2019-07-11 16:31:12 -0400

    updates boxplot

--- R/plotGeneBoxPPTC.R
+++ R/plotGeneBoxPPTC.R
@@ -37,41 +37,63 @@ plotGeneBoxPPTC <- function(gene1, tumor, myDataExp, myDataAnn, log, customtheme
   # add annotation data to expression set
   myDataExp.c <- merge(myDataExp.c, myDataAnn, by = "MODEL")
 
-  if(length(tumor) > 2) {
-    anovaRes <- aov(lm(myDataExp.c[,gene1]~myDataExp.c[,colorby]))
-    dat.tukey <- TukeyHSD(anovaRes)[[1]]
-    p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut, fill=colorby)) + 
-      geom_boxplot() + customtheme + 
-      ggtitle(gene1) + 
-      theme(axis.text.y = element_text(size = 12), 
-            axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 12), 
-            legend.position = "none")
-  } else if(length(tumor) == 2) {
-    anovaRes <- aov(lm(myDataExp.c[,gene1]~myDataExp.c[,colorby]))
-    dat.tukey <- data.frame()
-    pval <- summary(anovaRes)[[1]][[5]][1]
-    pval <- signif(pval, 6)
-    myText <- paste("Anova P-Val=", pval, sep="")
-    p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut, fill=colorby)) + 
-      geom_boxplot() + customtheme + 
-      ggtitle(paste0(gene1,'\n',myText)) + 
-      theme(axis.text.y = element_text(size = 12), 
-            axis.text.x = element_text(size = 12), 
-            legend.position = "none")
-  } else if(length(tumor) == 1) {
-    p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut, fill=colorby)) + 
-      customtheme + geom_boxplot() + 
-      ggtitle(gene1) +
-      theme(axis.text.y = element_text(size = 12), 
-            axis.text.x = element_text(size = 12),
-            axis.title.y = element_text(size = 12),
-            legend.position = "none")
-    dat.tukey <- data.frame()
+  # if(length(tumor) > 2) {
+  #   anovaRes <- aov(lm(myDataExp.c[,gene1]~myDataExp.c[,colorby]))
+  #   dat.tukey <- TukeyHSD(anovaRes)[[1]]
+  #   p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut, fill=colorby)) + 
+  #     geom_boxplot() + customtheme + 
+  #     ggtitle(gene1) + 
+  #     theme(axis.text.y = element_text(size = 12), 
+  #           axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 12), 
+  #           legend.position = "none")
+  # } else if(length(tumor) == 2) {
+  #   anovaRes <- aov(lm(myDataExp.c[,gene1]~myDataExp.c[,colorby]))
+  #   dat.tukey <- data.frame()
+  #   pval <- summary(anovaRes)[[1]][[5]][1]
+  #   pval <- signif(pval, 6)
+  #   myText <- paste("Anova P-Val=", pval, sep="")
+  #   p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut, fill=colorby)) + 
+  #     geom_boxplot() + customtheme + 
+  #     ggtitle(paste0(gene1,'\n',myText)) + 
+  #     theme(axis.text.y = element_text(size = 12), 
+  #           axis.text.x = element_text(size = 12), 
+  #           legend.position = "none")
+  # } else if(length(tumor) == 1) {
+  #   p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut, fill=colorby)) + 
+  #     customtheme + geom_boxplot() + 
+  #     ggtitle(gene1) +
+  #     theme(axis.text.y = element_text(size = 12), 
+  #           axis.text.x = element_text(size = 12),
+  #           axis.title.y = element_text(size = 12),
+  #           legend.position = "none")
+  #   dat.tukey <- data.frame()
+  # }
+  if(length(tumor) <= 2){
+    method = "t.test"
+  } else {
+    method = "anova"
   }
+  p <- ggplot(myDataExp.c, aes_string(x=colorby, y=gene1.mut)) + 
+    stat_boxplot(geom ='errorbar', width = 0.2) +
+    geom_boxplot(lwd = 0.5, fatten = 0.7, outlier.shape = 1, width = 0.5, outlier.size = 1, aes_string(fill = colorby)) +
+    geom_jitter(width = 0.1, pch = 21, stroke = 0.2, aes_string(fill = colorby)) + 
+    customtheme + 
+    theme(axis.text.x = element_blank()) +
+    ggtitle(gene1) +
+    stat_compare_means(method = method, label.x.npc = "center", label.y.npc = "top", color = "red") + 
+    stat_compare_means(label = "p.signif", ref.group = ".all.", color = "red", hide.ns = TRUE)
+  
   p <- plotly_build(p)
   p$x$layout$yaxis$title <- y.axis
   p$x$layout$xaxis$title <- ""
   
-  newList <- list(p, dat.tukey)
-  return(newList)
+  # remove outliers
+  p$x$data[1:length(levels(myDataExp.c[,colorby])) + 1] <- lapply(p$x$data[1:length(levels(myDataExp.c[,colorby])) + 1], FUN = function(x){
+    x$marker = list(opacity = 0)
+    return(x)
+  })
+  
+  # newList <- list(p, dat.tukey)
+  # return(newList)
+  return(p)
 }
